b7b195ccaded17db037ed77eebe2b74ccc9408fb
navihx <932942142@qq.com>
2021-02-05 16:43:25 +0800

    修改构建脚本: 使用qmake

--- build.sh
+++ build.sh
@@ -1,19 +1,8 @@
 mkdir build
 cd ./build
-
-FIND=$(ldconfig -p | grep libtag)
-if [ "$FIND" = "" ];
-then
-    say="CANNOT find taglib. Only build qmcdump."
-    cmake ..
-else
-    say="Taglib Found. Build both qmcdump and ncmdump."
-    cmake .. -DFIND=true
-fi
-
+qmake ..
 make
 
+mv vip-music-decoder ..
 cd ..
-mv ./build/*dump .
-rm -r ./build
-echo "$say"
+rm -r build
